c12906d4181a185db6de00e4a1dc11897a1d4718
Christian Weiske <cweiske@cweiske.de>
2012-03-27 07:33:10 +0200

    remove dead code

--- www/display.php
+++ www/display.php
@@ -8,30 +8,10 @@ require_once 'www-header.php';
 $repo = new Repository();
 $repo->loadFromRequest();
 
-/*
-$tplFiles = array();
-foreach ($files as $file) {
-    $tplFile = array();
-    $tplFile['filename'] = basename($file);
-    $tplFile['type'] = get_type_from_file($file);
-    //FIXME: highlight
-    $tplFile['content'] = file_get_contents($file);
-    $tplFile['raw'] = '/' . $id . '/raw/' . $tplFile['filename'];
-    $tplFiles[] = $tplFile;
-}
-*/
-
 render(
     'display',
     array(
         'repo' => $repo,
-        /*
-        'description' => file_get_contents($repoDir . '/.git/description'),
-        'files' => $tplFiles,
-        'links' => array(
-            'edit' => '/' . $id . '/edit'
-        )
-        */
     )
 );
 ?>
