c85fb1700ce938f28da420af9636e246912a996b
Christian Weiske <cweiske@cweiske.de>
2012-09-19 23:55:07 +0200

    implement request #13: simple remote forking works now

--- www/fork-remote.php
+++ www/fork-remote.php
@@ -6,19 +6,40 @@ namespace phorkie;
  */
 require_once 'www-header.php';
 
+$error = null;
+$urls  = null;
 if (isset($_POST['remote_url'])) {
     $fr = new ForkRemote($_POST['remote_url']);
-    $fr->parse();
-    if ($fr->hasUniqueGitUrl()) {
-        //FIXME: fork
+    if (false === $fr->parse()) {
+        //no url found
+        $error = 'No git:// clone URL found';
+    } else if (false !== ($gitUrl = $fr->getUniqueGitUrl())) {
+        $forker = new Forker();
+        $new    = $forker->forkRemote($gitUrl);
+        redirect($new->getLink('display'));
+    } else {
+        //multiple urls found
+        $urls = $fr->getGitUrls();
     }
-    //FIXME: display error or selection list
+}
+
+$selsize = 0;
+if (is_array($urls)) {
+    foreach ($urls as $group) {
+        ++$selsize;
+        if (count($group) > 1) {
+            $selsize += count($group);
+        }
+    }
 }
 
 render(
     'fork-remote',
     array(
-        'remote_url' => isset($_POST['remote_url']) ? $_POST['remote_url'] : ''
+        'remote_url' => isset($_POST['remote_url']) ? $_POST['remote_url'] : '',
+        'error'      => $error,
+        'urls'       => $urls,
+        'urlselsize' => $selsize,
     )
 );
 ?>
