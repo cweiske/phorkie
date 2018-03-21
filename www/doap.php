0bf21fac824008387412208dd6b0da08827efbed
Christian Weiske <cweiske@cweiske.de>
2018-03-21 23:26:05 +0100

    Fix E_NOTICE: Only variables should be passed by reference

--- www/doap.php
+++ www/doap.php
@@ -10,12 +10,14 @@ require_once 'www-header.php';
 $repo = new Repository();
 $repo->loadFromRequest();
 
+$history = $repo->getHistory();
+
 header('Content-Type: application/rdf+xml');
 render(
     'doap',
     array(
         'repo' => $repo,
-        'date' => date('Y-m-d', end($repo->getHistory())->committerTime),
+        'date' => date('Y-m-d', end($history)->committerTime),
         'link' => Tools::fullUrl($repo->getLink('display'))
     )
 );
